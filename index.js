e0a258d3eb099b20f09edda18b8e10a63ad87ee5
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2025-04-09 11:57:11 +0000

    express server setup

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from 'cors'
+import bodyParser from "body-parser";
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded());
+
+
+app.get("/", (req, res) => {
+    res.json({ "data": "Hi I am running" });
+})
+
+app.post("/add", (req, res) => {
+    console.log("Data jo aaya ------->", req.body);
+    res.status(201).json({"data" : "Posted Successfully"});
+});
+
+
+app.listen(8080, (req, res) => {
+    console.log("Server up and running on " + process.env.DEV_PORT);
+})
