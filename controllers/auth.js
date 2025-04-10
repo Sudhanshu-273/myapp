a82e467fa41cf86d1a7073250c3ca2df8ff33670
Rahul Kumar <79074470+rahullkr@users.noreply.github.com>
2025-04-10 10:28:18 +0000

    login setup halfway

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,35 @@
-export const login = (req, res) => {
+import { QueryError } from "sequelize";
+import { sequelize } from "../db.config.js";
+
+export const user_data = async (req,res) => {
+    const [data] = await sequelize.query("SELECT * FROM users");
+    console.log(data);
+    res.json({"data": data});
+
+}
+export const login = async (req, res) => {
     res.send("Logged in");
+    console.log(body);
+    try {
+        const user = user_data.data;
+        console.log(user)
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message: "User not Found.",
+            })
+        }
+        // const passwordMatch 
+        
+    } catch(error)
+    {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success:false,
+            message: "some error occured",
+        })
+    }
 }
 
 export const register = async (req, res) => {
@@ -7,7 +37,4 @@ export const register = async (req, res) => {
     if (password !== confirmPassword) {
         return res.status(400).json({ message: "Passwords do not match" });
     }
-
-    
-
 }
