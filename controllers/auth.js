5656d8f1314e28061aa485753ef53c4dfa91f967
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2025-04-10 10:58:55 +0000

    added login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-import { QueryError } from "sequelize";
+import { QueryError, Sequelize } from "sequelize";
 import { sequelize } from "../db.config.js";
-import {bcrypt} from 'bcrypt'
+// import {bcrypt} from 'bcrypt'
 
 export const user_data = async (req,res) => {
     const [data] = await sequelize.query("SELECT * FROM users");
@@ -8,11 +8,14 @@ export const user_data = async (req,res) => {
     res.json({"data": data});
 
 }
+
+
 export const login = async (req, res) => {
-    res.send("Logged in successfully");
-    console.log(body);
+    // res.send("Logged in successfully");
+    // console.log(body);
+    const user = req.body;
     try {
-        const user = user_data.data;
+        // const user = req.user;
         console.log(user)
         if(!user){
             return res.status(404).json({
@@ -20,16 +23,46 @@ export const login = async (req, res) => {
                 message: "User not Found.",
             })
         }
-        const passwordMatch = bcrypt.compareSync(
-            password.toString(),
-            user.password
-          );
-          if (!passwordMatch) {
+        const { email, password } = user;
+
+        const [[data], m1] = await sequelize.query("select * from users where email = :email", {
+            replacements: {
+                email: email,
+            }
+        })
+
+        console.log(data);
+
+        const user_id = data.id;
+
+        // check hashed password here
+
+        if(password !== data.password) {
             return res.status(401).json({
-              success: false,
-              message: "Incorrect password",
+                success: false,
+                message: "Incorrect password",
             });
-          }
+        }
+
+        res.status(200).json( {
+            success: true,
+            message: "Login successful",
+            data: {
+                id: user_id,
+                email: email,
+            },
+        })
+
+        // const passwordMatch = bcrypt.compareSync(
+        //     password.toString(),
+        //     user.password
+        //   );
+        //   if (!passwordMatch) {
+        //     return res.status(401).json({
+        //       success: false,
+        //       message: "Incorrect password",
+        //     });
+        //   }
       
           return res.status(200).json({
             success: true,
