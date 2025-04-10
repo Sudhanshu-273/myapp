24c20b71f5d1520b2ea37ce34fa8ed7896860c0a
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2025-04-10 10:34:08 +0000

    setup middleware auth

--- controllers/home.js
+++ controllers/home.js
@@ -7,5 +7,16 @@ export const home = async (req, res) => {
         }
     });
     console.log(data)
-    res.json({"data" : data});
+    res.json({ "data": data });
+}
+
+export const getAllEquipment = async (req, res) => {
+    try {
+        const [data] = await sequelize.query("SELECT * FROM gym_equipments");
+        res.json({ "data": data });
+    } catch (error) {
+        console.error("Error fetching equipment data: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+
 }
