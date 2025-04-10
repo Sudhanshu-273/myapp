4a40efe6b01cf203d809d0ec5adf056f00256548
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2025-04-10 09:53:02 +0000

    creating queries

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,11 @@
 import { sequelize } from "../db.config.js";
 
 export const home = async (req, res) => {
-    const [data] = await sequelize.query("SELECT * FROM muscle_group");
+    const [data] = await sequelize.query("SELECT * FROM muscle_group where id = :id", {
+        replacements: {
+            id: 1,
+        }
+    });
     console.log(data)
     res.json({"data" : data});
 }
