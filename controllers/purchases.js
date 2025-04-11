fcf3890c9ad4f9bed724faaf7f33f8f5c86458f3
Sudhanshu Chaubey <sudhanshuchaubey273@gmail.com>
2025-04-11 07:48:22 +0000

    modified purchases

--- controllers/purchases.js
+++ controllers/purchases.js
@@ -1,6 +1,6 @@
 import { sequelize } from "../db.config.js";
 
 export const addPurchase = async (req, res) => {
-    const { product_id, price, quantity } = req.body;
+    const { product_id, product_name, price, quantity, newProduct } = req.body;
     res.send(req.body);
 }
