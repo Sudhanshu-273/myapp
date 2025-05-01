5c8b15add00496f51ec51462295fac7f31ba3043
Sudhanshu <sudhanshuchaubey273@gmail.com>
2025-05-01 10:04:54 +0000

    modified plans

--- controllers/admin/plans.js
+++ controllers/admin/plans.js
@@ -1,37 +1,15 @@
 import { sequelize } from "../../db.config.js";
 
 export const getPlan = async (req, res) => {
-  const { name, duration } = req.body;
-
-  if (!name || !duration) {
-    return res.status(400).json({ message: "Name and duration are required." });
-  }
-
   try {
-    const [results] = await sequelize.query(
-      `
-     select price from plans 
-      `,
-      {
-        replacements: { name, duration },
-        type: sequelize.QueryTypes.SELECT,
-      }
-    );
-    console.log(results.price);
-    if (!results || results.length === 0) {
-      return res.status(404).json({ message: "No matching plan found." });
-    }
-
-    res.status(200).json({
-      message: "Plan details fetched successfully",
-      price: results.price
-    });
-  } catch (error) {
-    console.error("Error fetching plan details:", error);
-    res.status(500).json({
-      message: "Failed to fetch plan details",
-      error: error.message,
-    });
+    const getPlansSQL = "select p.id as id, p.duration, p.price, p.active, pt.name as plan_type from plans p inner join plan_types pt on p.plan_type = pt.id";
+    const [plans] = await sequelize.query(getPlansSQL);
+    const getDescriptionsSQL = "select name as plan_type, description from plan_types";
+    const [descriptions] = await sequelize.query(getDescriptionsSQL);
+    res.status(200).json({ plans: plans, descriptions: descriptions });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error", error: err });
   }
 };
 
@@ -74,6 +52,7 @@ export const addPlan = async (req, res) => {
     console.error("Error adding plan:", error);
     res.status(500).json({ message: "Failed to add plan", error: error.message });
   }
+
 };
 
 
