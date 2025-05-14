ca206307b491ea86502b57842364c38f6c6c2380
rahullkr <rk7318323176@gmail.com>
2025-05-14 06:19:13 +0000

    added edit api and fixed bugs

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { updateUser, forgotPassword, listUser } from '../controllers/user.js'
+import { updateUser, forgotPassword, listUser, edituser } from '../controllers/user.js'
 
 router.patch('/update', updateUser);
 router.patch('/forgotPassword', forgotPassword);
 router.get('/list',listUser);
-
+router.patch('/edit', edituser);
 export default router;
